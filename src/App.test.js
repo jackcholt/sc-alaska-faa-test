84b155f9c59bf9c8bffeab494e6b8e1ecfbc3bd6
Jack C. Holt <jackcholt@gmail.com>
2022-03-18 14:34:19 -0800

    Finish Flight Plan

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders bodies of water link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/bodies of water/i);
   expect(linkElement).toBeInTheDocument();
 });
